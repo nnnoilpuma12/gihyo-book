6eb1e201c641f6d8d064efdf0025e9ab57ca93af
Naiki Watanabe <nnnoilpuma12@gmail.com>
2022-08-15 22:59:25 +0900

    レスポンシブ対応の下準備

--- next.config.js
+++ next.config.js
@@ -2,9 +2,31 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    // styledcomponentsの有効化
-    styledComponents: true,
+  compiler: (() => {
+    let compilerConfig = {
+      // styledcomponentsの有効化
+      styledComponents: true,
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdata-testidを削除
+        reactRemoveProperties: { properties: ["^data-testid$"] },
+      };
+    }
+
+    return compilerConfig;
+  })(),
+  async rewrites() {
+    return [
+      {
+        // ex. /api/proxy
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        // ex. http://localhost:8000
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ];
   },
 };
 
